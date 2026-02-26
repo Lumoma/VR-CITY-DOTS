82b7c1c1251bb7d489d3abe8e2f68480b03dbfa1
Lukas Morawietz <lukas.morawietz@stud.hochschule-heidelberg.de>
2026-02-26 14:51:46 +0100

    Add .nojekyll to disable Jekyll processing

--- Assets/Docs/html/menudata.js
+++ Assets/Docs/html/menudata.js
@@ -29,7 +29,7 @@ var menudata={children:[
 {text:"Klassen",url:"annotated.html",children:[
 {text:"Auflistung der Klassen",url:"annotated.html"},
 {text:"Klassen-Verzeichnis",url:"classes.html"},
-{text:"Klassenhierarchie",url:"hierarchy.html"},
+{text:"Klassenhierarchie",url:"inherits.html"},
 {text:"Klassen-Elemente",url:"functions.html",children:[
 {text:"Alle",url:"functions.html",children:[
 {text:"_",url:"functions.html#index__5F"},
